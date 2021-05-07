4a7408e50690a91d5f96ad2ec76c119974dd912a
wayangalihpratama <wgprtm@gmail.com>
2021-05-07 20:37:11 +0800

    Initial Setup

---
+++ ethiopia/resources/js/components/example.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
+                        <div className="card-body">
+                            I'm an example component! wgprtm
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
---
+++ ethiopia/resources/js/root.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Example from "./components/example";
+
+if (document.getElementById("root")) {
+    ReactDOM.render(<Example />, document.getElementById("root"));
+}
