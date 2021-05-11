6ed0669e5052520bee7304b8d519eafa83c9ad9f
wayangalihpratama <wgprtm@gmail.com>
2021-05-11 12:24:56 +0800

    [#1] Add favicon

--- ethiopia/resources/views/index.blade.php
+++ ethiopia/resources/views/index.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="shortcut icon" type="image/jpg" href="/images/wai-logo.png"/>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
