35736b36282a0917d61bb252bd28c2581b587f9e
Iván Perdomo <ivan@perdomo.me>
2021-05-13 18:47:10 +0200

    [#10] Initial build for ethiopia/ folder

---
+++ ethiopia/ci/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
+       --workdir /home/tcakvo/public_html/unep-dashboard \
+       --entrypoint /bin/sh \
+       composer:2.0.7 -c 'composer install'
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
+       --workdir /home/tcakvo/public_html/unep-dashboard \
+       --entrypoint /bin/sh \
+       composer:2.0.7 -c 'composer dump-autoload'
+
+echo 'MIX_PUBLIC_URL="https://wai-mis.tc.akvo.org"' > .env
+
+docker run \
+       --rm \
+       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
+       --workdir "/home/tcakvo/public_html/unep-dashboard" \
+       --entrypoint /bin/sh \
+       node:14-alpine -c 'npm install && npm run prod'
