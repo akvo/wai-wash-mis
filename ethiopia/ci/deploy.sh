824a8b0ef4c8258665c66777909de5d36f255882
Iván Perdomo <ivan@perdomo.me>
2021-05-13 19:11:48 +0200

    [#10] Initial attempt to deploy

---
+++ ethiopia/ci/deploy.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2039
+
+set -eux
+
+# Change owner as docker generated files
+# are usually owned by root
+sudo chown "${USER}:" . -R
+
+echo "Deploying site..."
+
+if [[ "${CI_BRANCH}" != "main" ]]; then
+    exit 0
+fi
+
+if [[ "${CI_PULL_REQUEST}" == "true" ]]; then
+    exit 0
+fi
+
+base_folder="www/tc.akvo.org/public_html/wai-mis/ethiopia"
+
+rsync \
+    --archive \
+    --compress \
+    --progress \
+    --exclude=ci \
+    --exclude=node_modules \
+    --rsh="ssh -i ${SITES_SSH_KEY} -o BatchMode=yes -p 18765 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+    . "u7-nnfq7m4dqfyx@35.214.170.100:/home/customer/${base_folder}"
+
+remote_exec () {
+    ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+	-p 18765 \
+	-o UserKnownHostsFile=/dev/null \
+	-o StrictHostKeyChecking=no \
+	u7-nnfq7m4dqfyx@35.214.170.100 "$@"
+}
+
+remote_exec \
+    "find ${base_folder}/ -not -path '*.well-known*' -type f -print0 | xargs -0 -n1 chmod 644"
+
+remote_exec \
+    "find ${base_folder}/ -type d -print0 | xargs -0 -n1 chmod 755"
+
+# echo "Copy the config..."
+# ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+#     -p 18765 \
+#     -o UserKnownHostsFile=/dev/null \
+#     -o StrictHostKeyChecking=no \
+#     u7-nnfq7m4dqfyx@35.214.170.100 "cp ~/env/${FOLDER}.env.prod ${base_folder}/.env"
+
+# echo "Clearing cache..."
+
+# ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+#     -p 18765 \
+#     -o UserKnownHostsFile=/dev/null \
+#     -o StrictHostKeyChecking=no \
+#     u7-nnfq7m4dqfyx@35.214.170.100 "cd ${base_folder}/ && /usr/local/bin/php73 artisan cache:clear"
+
+# ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+#     -p 18765 \
+#     -o UserKnownHostsFile=/dev/null \
+#     -o StrictHostKeyChecking=no \
+#     u7-nnfq7m4dqfyx@35.214.170.100 "cd ${base_folder}/ && /usr/local/bin/php73 artisan l5-swagger:generate"
+
+# ssh -i "${SITES_SSH_KEY}" -o BatchMode=yes \
+#     -p 18765 \
+#     -o UserKnownHostsFile=/dev/null \
+#     -o StrictHostKeyChecking=no \
+#     u7-nnfq7m4dqfyx@35.214.170.100 "cd www/tc.akvo.org/public_html/${FOLDER}/ && /usr/local/bin/composer dump-autoload"
+
+echo "Done"
