309c875205230deec973d01dcddc377277bfd4c5
Iván Perdomo <ivan@perdomo.me>
2021-05-13 18:50:25 +0200

    [#10] Adjust folder paths

--- ethiopia/ci/build.sh
+++ ethiopia/ci/build.sh
@@ -4,15 +4,15 @@ set -eu
 
 docker run \
        --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
-       --workdir /home/tcakvo/public_html/unep-dashboard \
+       --volume "$(pwd):/home/tcakvo/public_html/wai-mis" \
+       --workdir /home/tcakvo/public_html/wai-mis \
        --entrypoint /bin/sh \
        composer:2.0.7 -c 'composer install'
 
 docker run \
        --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
-       --workdir /home/tcakvo/public_html/unep-dashboard \
+       --volume "$(pwd):/home/tcakvo/public_html/wai-mis" \
+       --workdir /home/tcakvo/public_html/wai-mis \
        --entrypoint /bin/sh \
        composer:2.0.7 -c 'composer dump-autoload'
 
@@ -20,7 +20,7 @@ echo 'MIX_PUBLIC_URL="https://wai-mis.tc.akvo.org"' > .env
 
 docker run \
        --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/unep-dashboard" \
-       --workdir "/home/tcakvo/public_html/unep-dashboard" \
+       --volume "$(pwd):/home/tcakvo/public_html/wai-mis" \
+       --workdir "/home/tcakvo/public_html/wai-mis" \
        --entrypoint /bin/sh \
        node:14-alpine -c 'npm install && npm run prod'
