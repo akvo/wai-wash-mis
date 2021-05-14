b450fd41fa8a91535616442ce43177c70b07abd3
Iván Perdomo <ivan@perdomo.me>
2021-05-14 09:34:01 +0200

    [#10] Deploy to subdomain

--- ethiopia/ci/build.sh
+++ ethiopia/ci/build.sh
@@ -4,23 +4,23 @@ set -eu
 
 docker run \
        --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/wai-mis" \
-       --workdir /home/tcakvo/public_html/wai-mis \
+       --volume "$(pwd):/home/tcakvo/public_html/eth-wai-mis" \
+       --workdir /home/tcakvo/public_html/eth-wai-mis \
        --entrypoint /bin/sh \
        composer:2.0.7 -c 'composer install'
 
 docker run \
        --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/wai-mis" \
-       --workdir /home/tcakvo/public_html/wai-mis \
+       --volume "$(pwd):/home/tcakvo/public_html/eth-wai-mis" \
+       --workdir /home/tcakvo/public_html/eth-wai-mis \
        --entrypoint /bin/sh \
        composer:2.0.7 -c 'composer dump-autoload'
 
-echo 'MIX_PUBLIC_URL="https://wai-mis.tc.akvo.org/ethiopia"' > .env
+echo 'MIX_PUBLIC_URL="https://eth-wai-mis.tc.akvo.org/ethiopia"' > .env
 
 docker run \
        --rm \
-       --volume "$(pwd):/home/tcakvo/public_html/wai-mis" \
-       --workdir "/home/tcakvo/public_html/wai-mis" \
+       --volume "$(pwd):/home/tcakvo/public_html/eth-wai-mis" \
+       --workdir "/home/tcakvo/public_html/eth-wai-mis" \
        --entrypoint /bin/sh \
        node:14-alpine -c 'npm install && npm run prod'
