b30cdce7d93a9b755893cba1227f44f4f8981251
wayangalihpratama <wgprtm@gmail.com>
2021-07-14 13:55:21 +0800

    [#15] Initial duplicate of WAI - Nepal

---
+++ nepal/resources/js/pages/details/household.js
@@ -0,0 +1,244 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactECharts from "echarts-for-react";
+import { Col, Menu, Row, Divider } from "antd";
+
+import Map from "../../components/maps";
+import DetailTable from "../../components/detail-table";
+
+import { jmpColors } from "../../utils/jmp_color";
+import { UIStore } from "../../store";
+
+import groupBy from "lodash/groupBy";
+import sortBy from "lodash/sortBy";
+import sumBy from "lodash/sumBy";
+import camelCase from "lodash/camelCase";
+import reverse from "lodash/reverse";
+
+const indicatorRank = {
+    safelyManaged: 15,
+    basic: 10,
+    limited: 1,
+    unimproved: -1,
+    surfaceWater: -2,
+    openDefecation: -2,
+    noFacility: -2,
+};
+
+const generateChartOptions = (config, data, level2Key, firstFilter, level2) => {
+    data = data.filter((x) => x[level2Key] !== "");
+    const locations = Object.keys(groupBy(data, level2Key));
+    const options = config?.charts.map((item) => {
+        data = data.filter((x) => item.value.includes(x[item.key]));
+        let option = {
+            tooltip: {
+                trigger: "axis",
+                position: ["80%", "25%"],
+                axisPointer: {
+                    type: "shadow",
+                },
+                extraCssText: "text-align:left;",
+            },
+            legend: {
+                data: item.value,
+            },
+            grid: {
+                left: "7%",
+                right: "7%",
+                bottom: "10%",
+                containLabel: true,
+            },
+            yAxis: {
+                type: "category",
+                data: locations.map((x) => ({
+                    value: x,
+                    textStyle: { fontSize: 14 },
+                })),
+            },
+            xAxis: {
+                type: "value",
+                nameLocation: "middle",
+                nameGap: 45,
+                max: 100,
+                nameTextStyle: {
+                    fontWeight: "bold",
+                    fontSize: 14,
+                },
+            },
+            series: [],
+        };
+
+        const jmpColor = jmpColors[firstFilter];
+        const itemColors = jmpColor[item.name.split(" ")[0].toLowerCase()];
+
+        const dataByTopic = item.value.map((val) => {
+            const topic = data.filter((x) => x[item.key] === val);
+            return {
+                name: val,
+                values: topic,
+            };
+        });
+        let rankedTopic = [];
+        const seriesData = dataByTopic.map((topic) => {
+            const dataByLocation = locations.map((loc) => {
+                const val = topic.values.filter((x) => x[level2Key] === loc);
+                const totalDatalevel2 = data.filter(
+                    (x) => x[level2Key] === loc
+                );
+                const value =
+                    val.length !== 0
+                        ? (val.length / totalDatalevel2.length) * 100
+                        : 0;
+                let res = {
+                    name: loc,
+                    value: Math.round((value + Number.EPSILON) * 100) / 100,
+                };
+                if (level2 && level2 !== loc.toLowerCase()) {
+                    res = {
+                        ...res,
+                        itemStyle: {
+                            opacity: 0.25,
+                        },
+                    };
+                }
+                return res;
+            });
+
+            const itemColor = itemColors.find(
+                (c) => c.name.toLowerCase() === topic.name.toLowerCase()
+            );
+            const series = {
+                name: topic.name,
+                type: "bar",
+                stack: "data",
+                data: dataByLocation,
+                itemStyle: {
+                    color: itemColor?.color,
+                },
+            };
+
+            const topicRank = indicatorRank[camelCase(topic.name)];
+            if (topicRank) {
+                sortBy(dataByLocation, "value").forEach((x, i) => {
+                    rankedTopic.push({
+                        name: x.name,
+                        rank: topicRank * x.value,
+                    });
+                });
+            }
+
+            let collectedRank = groupBy(rankedTopic, "name");
+            collectedRank = Object.keys(collectedRank).map((x) => {
+                const val = sumBy(collectedRank[x], (o) => o.rank);
+                return { name: x, rank: val };
+            });
+            collectedRank = reverse(sortBy(collectedRank, "rank"));
+            option["yAxis"]["data"] = collectedRank.map((x) => ({
+                value: x.name,
+                textStyle: { fontSize: 14 },
+            }));
+
+            option["series"] = [...option.series, series];
+            return seriesData;
+        });
+
+        const sortedLocation = option.yAxis.data.map((x) => x.value);
+        option["series"] = option.series.map((x) => {
+            let sortedValue = sortedLocation.map((l) => {
+                return x.data.find((d) => d.name === l);
+            });
+            return {
+                ...x,
+                data: sortedValue,
+            };
+        });
+
+        return { name: item.name, option: option };
+    });
+    return options;
+};
+
+const HouseHold = ({ geoUrl }) => {
+    const store = UIStore.useState();
+    const { level1, level2, state, firstFilter, secondFilter } = store;
+    const [chartOptions, setChartOptions] = useState();
+    const chartsRef = useRef([]);
+
+    useEffect(() => {
+        setChartOptions(null);
+        const { data, config } = state;
+        const level1Key = config?.locations?.level1;
+        const level2Key = config?.locations?.level2;
+
+        let filterData = data;
+        // filter data by level1
+        if (level1) {
+            filterData = filterData.filter(
+                (x) => x[level1Key].toLowerCase() === level1.toLowerCase()
+            );
+        }
+
+        // generate charts
+        let options = generateChartOptions(
+            config,
+            filterData,
+            level2Key,
+            firstFilter,
+            level2
+        );
+        if (secondFilter !== "all") {
+            options = options.filter((x) =>
+                x.name.toLowerCase().includes(secondFilter.toLowerCase())
+            );
+        }
+        setChartOptions(options);
+    }, [firstFilter, secondFilter, level1, level2]);
+
+    const onChartsClick = (params, index) => {
+        const echartInstance = chartsRef.current[index].getEchartsInstance();
+        UIStore.update((e) => {
+            e.level2 = params.data.name.toLowerCase();
+            e.markerDetail = {
+                ...e.markerDetail,
+                active: false,
+                data: {},
+            };
+        });
+    };
+
+    return (
+        <Row>
+            <Col span="24">
+                {/* {geoUrl && (
+                    <div key="maps" className="map-container">
+                        <Map geoUrl={geoUrl} />
+                    </div>
+                )} */}
+                {geoUrl &&
+                    chartOptions &&
+                    chartOptions.map((opt, index) => (
+                        <div key={opt.name} className="chart-container">
+                            <h4>{opt.name}</h4>
+                            <Divider />
+                            <ReactECharts
+                                option={opt.option}
+                                onEvents={{
+                                    click: (e) => onChartsClick(e, index),
+                                }}
+                                style={{
+                                    height: opt.option.series?.[0]?.data
+                                        ? opt.option.series[0].data.length * 90
+                                        : 600,
+                                }}
+                                ref={(ref) => {
+                                    chartsRef.current[index] = ref;
+                                }}
+                            />
+                        </div>
+                    ))}
+                <DetailTable />
+            </Col>
+        </Row>
+    );
+};
+
+export default HouseHold;
---
+++ nepal/resources/js/store.js
@@ -0,0 +1,60 @@
+import { Store } from "pullstate";
+
+import hhData from "./data/nep_hh_data.json";
+import hhConfig from "./data/nep_hh_config.json";
+
+import schoolData from "./data/nep_school_data.json";
+import schoolConfig from "./data/nep_school_config.json";
+
+import healthData from "./data/nep_health_data.json";
+import healthConfig from "./data/nep_health_config.json";
+
+// import cltsData from "./data/ug_clts_data.json";
+// import cltsConfig from "./data/ug_clts_config.json";
+
+import wpData from "./data/ug_wp_data.json";
+import wpConfig from "./data/ug_wp_config.json";
+
+let cltsData, cltsConfig;
+
+export const UIStore = new Store({
+    hh: {
+        data: hhData,
+        config: hhConfig,
+    },
+    school: {
+        data: schoolData,
+        config: schoolConfig,
+    },
+    health: {
+        data: healthData,
+        config: healthConfig,
+    },
+    clts: {
+        data: cltsData,
+        config: cltsConfig,
+    },
+    wp: {
+        data: wpData,
+        config: wpConfig,
+    },
+    state: {
+        data: [],
+        config: {},
+        charts: null,
+        tables: null,
+    },
+    page: "home",
+    level1: null,
+    level2: null,
+    level3: null,
+    firstFilter: "hh",
+    secondFilter: "all",
+    level1List: [],
+    level2List: [],
+    level3List: [],
+    markerDetail: {
+        active: false,
+        data: {},
+    },
+});
